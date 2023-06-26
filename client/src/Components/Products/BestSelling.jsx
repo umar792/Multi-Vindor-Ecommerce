90610ba8336062ae57fb9517142aa9b64f465e75
Mbilalshafqat <106830172+Umar792@users.noreply.github.com>
2023-06-27 00:00:52 +0500

    last one

--- client/src/Components/Products/BestSelling.jsx
+++ client/src/Components/Products/BestSelling.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { productData } from "../../DataStatic/Data";
 import AllProductrView from "../AllProductView/AllProductrView";
+import { useSelector } from "react-redux";
+import DashboardAllProductView from "../SellerPages/SellerDashboard/DashboardAllProductView/DashboardAllProductView";
 
 const BestSelling = () => {
-  const SellingProducts = productData.filter((item) => {
-    return item.total_sell > 30;
-  });
+  const AllProductsData = useSelector((state) => state.owner.AllProductsData);
+  const SellingProducts =
+    AllProductsData &&
+    AllProductsData.filter((item) => {
+      return item.sold_out > 15;
+    });
+
+  console.log(AllProductsData);
+
   return (
     <>
       <h2 className="text-center font-bold text-3xl my-3">
         Best Selling Products
       </h2>
       <p>Total Prducts: {SellingProducts && SellingProducts.length}</p>
-      <AllProductrView data={SellingProducts && SellingProducts} />
+      <DashboardAllProductView data={SellingProducts && SellingProducts} />
     </>
   );
 };
--- client/src/Components/Products/Products.jsx
+++ client/src/Components/Products/Products.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { productData } from "../../DataStatic/Data";
 import AllProductrView from "../AllProductView/AllProductrView";
+import { useSelector } from "react-redux";
+import DashboardAllProductView from "../SellerPages/SellerDashboard/DashboardAllProductView/DashboardAllProductView";
 
 const Products = () => {
+  const AllProductsData = useSelector((state) => state.owner.AllProductsData);
+
   return (
     <>
       <h2 className="text-center font-bold text-3xl my-3">All Products</h2>
-      <p>Total Prducts: {productData && productData.length}</p>
-      <AllProductrView data={productData} />
+      <p>Total Prducts: {AllProductsData && AllProductsData.length}</p>
+      <DashboardAllProductView data={AllProductsData} />
     </>
   );
 };
